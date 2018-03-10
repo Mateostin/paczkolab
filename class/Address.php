39a339471f9f9ed25865faba13301c5d6e88a7d4
kordianpiatek <kordian55>
2018-03-10 14:48:29 +0100

    Address

--- class/Address.php
+++ class/Address.php
@@ -13,4 +13,118 @@ class Address implements Action
     {
 
     }
+
+    public function save()
+    {
+        // TODO: Implement save() method.
+    }
+
+    public function update()
+    {
+        // TODO: Implement update() method.
+    }
+
+    public function delete()
+    {
+        // TODO: Implement delete() method.
+    }
+
+    public static function load($id = null)
+    {
+        // TODO: Implement load() method.
+    }
+
+    public static function loadAll()
+    {
+        // TODO: Implement loadAll() method.
+    }
+
+    public static function setDb(Database $db)
+    {
+        // TODO: Implement setDb() method.
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * @param mixed $city
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPostcode()
+    {
+        return $this->postcode;
+    }
+
+    /**
+     * @param mixed $postcode
+     */
+    public function setPostcode($postcode)
+    {
+        $this->postcode = $postcode;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStreet()
+    {
+        return $this->street;
+    }
+
+    /**
+     * @param mixed $street
+     */
+    public function setStreet($street)
+    {
+        $this->street = $street;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getHomeNumber()
+    {
+        return $this->homeNumber;
+    }
+
+    /**
+     * @param mixed $homeNumber
+     */
+    public function setHomeNumber($homeNumber)
+    {
+        $this->homeNumber = $homeNumber;
+    }
+
+
+
+
 }
