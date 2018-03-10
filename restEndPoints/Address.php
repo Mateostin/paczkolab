553ff10ceec63144c09108f3383d5dbc9fee8dbd
kordianpiatek <kordian55>
2018-03-10 16:51:30 +0100

    endpoints

---
+++ restEndPoints/Address.php
@@ -0,0 +1,15 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    Address::setDb($db);
+    $addresses =Address::loadAll();
+    $tmpAddresses = [];
+    foreach ($addresses as $k => $address) {
+        $tmpAddresses[$k]['id'] = $address['id'];
+        $tmpAddresses[$k]['city'] = $address['city'];
+        $tmpAddresses[$k]['postcode'] = $address['postcode'];
+        $tmpAddresses[$k]['street'] = $address['street'];
+        $tmpAddresses[$k]['house'] = $address['house'];
+    }
+    $response = $tmpAddresses;
+}
