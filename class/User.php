4c17d75d27ef4dea6b8380ac655a0f57a05b5f4a
Mateusz <mat.lipecki@gmail.com>
2018-03-10 15:11:49 +0100

    Create getters and setter for User class

--- class/User.php
+++ class/User.php
@@ -9,6 +9,53 @@ class User implements Action
     private $creditQuantity;
     private $address;
 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getSurname()
+    {
+        return $this->surname;
+    }
+
+    public function setSurname($surname)
+    {
+        $this->surname = $surname;
+    }
+
+    public function getCreditQuantity()
+    {
+        return $this->creditQuantity;
+    }
+
+    public function setCreditQuantity($creditQuantity)
+    {
+        $this->creditQuantity = $creditQuantity;
+    }
+
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    public function setAddress($address)
+    {
+        $this->address = $address;
+    }
+
+
+
     public function save()
     {
 
