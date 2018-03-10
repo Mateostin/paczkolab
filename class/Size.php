bfd0380165d393d7da4f8a1c6accd2e59875885b
wysbea <wysbea@gmail.com>
2018-03-10 17:00:11 +0100

    test

--- class/Size.php
+++ class/Size.php
@@ -9,65 +9,54 @@ class Size implements Action
      * @var Database
      */
     public static $db;
-
     public function __construct($size, $price) {
         $this->size = $size;
         $this->price = $price;
     }
-
     public function getId(){
         return $this->id;
     }
-
     public function getSize(){
         return $this->size;
     }
-
     public function setSize($size){
         $this->size = $size;
-
         return $this;
     }
-
     public function getPrice(){
         return $this->price;
     }
-
     public function setPrice($price){
         $this->price = $price;
         return $this;
     }
-
     public function save()
     {
+        self::$db->query("INSERT INTO Size (size, price) VALUES (:size, :price)");
+        self::$db->bind(':size', $this->getSize());
+        self::$db->bind(':price', $this->getPrice());
+        self::$db->execute();
         // TODO: Implement save() method.
     }
-
     public function update()
     {
         // TODO: Implement update() method.
     }
-
     public function delete()
     {
         // TODO: Implement delete() method.
     }
-
     public static function load($id = null)
     {
         // TODO: Implement load() method.
     }
-
     public static function loadAll()
     {
         self::$db->query("SELECT * FROM Size");
         return self::$db->resultSet();
     }
-
     public static function setDb(Database $db)
     {
         self::$db = $db;
     }
-
-
 }
