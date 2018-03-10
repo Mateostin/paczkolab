b5d630cd8213b7f366c895a9643f80b83d1d699d
wysbea <wysbea@gmail.com>
2018-03-10 15:16:10 +0100

    test

--- class/Size.php
+++ class/Size.php
@@ -5,4 +5,56 @@ class Size implements Action
     private $id;
     private $size;
     private $price;
+
+    public function __construct($size, $price) {
+        $this->size = $size;
+        $this->price = $price;
+    }
+
+    public function getId(){
+        return $this->id;
+    }
+
+    public function getSize(){
+        return $this->size;
+    }
+
+    public function setSize($size){
+        $this->size = $size;
+
+        return $this;
+    }
+
+    public function getPrice(){
+        return $this->price;
+    }
+
+    public function setPrice($price){
+        $this->price = $price;
+        return $this;
+    }
+
+    public function loadFromDB($id)
+    {
+        //TODO:
+    }
+    public function saveToDB()
+    {
+        //TODO:
+    }
+
+    public function update()
+    {
+        //TODO:
+    }
+
+    public function deleteFromDB()
+    {
+        //TODO:
+    }
+
+    public static function loadAllFromDB() {
+        //TODO:
+    }
+
 }
