2b1bdcad7afdc8e47cfe02280204f4f5300ae382
kordianpiatek <kordian55>
2018-03-10 14:31:07 +0100

    Address

--- class/Address.php
+++ class/Address.php
@@ -2,6 +2,14 @@
 
 class Address implements Action
 {
+    private $id;
+    private $city;
+    private $postcode;
+    private $street;
+    private $homeNumber;
 
-    
+    public function __construct()
+    {
+        
+    }
 }
