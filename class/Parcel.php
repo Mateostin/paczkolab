14aca051afcb5bb018a28f6420170eb3bc5d0a05
kordianpiatek <kordian55>
2018-04-14 18:15:06 +0200

    parcel

--- class/Parcel.php
+++ class/Parcel.php
@@ -7,29 +7,61 @@ class Parcel implements Action
     private $size;
     private $address;
 
+    /**
+     * @var Database
+     */
+    public static $db;
+
+    public function __construct($sender,$size,$address)
+    {
+        $this->sender = $sender;
+        $this->size = $size;
+        $this->address = $address;
+
+    }
+
     public function save()
     {
-
+        self::$db->query('INSERT INTO Parcel(sender,size,address) VALUES (:sender,:size,:address)');
+        self::$db->bind('sender', $this->getSender());
+        self::$db->bind('size', $this->getSize());
+        self::$db->bind('address', $this->getAddress());
+        return self::$db->execute();
     }
 
     public function update($id = null)
     {
+        self::$db->query('UPDATE Parcel SET sender=:sender,size=:size, address=:address WHERE id=:id');
+        self::$db->bind('id', $id, null);
+        self::$db->bind('sender', $this->getSender());
+        self::$db->bind('size', $this->getSize());
+        self::$db->bind('address', $this->getAddress());
+        return self::$db->execute();
     }
 
     public static function delete($id = null)
     {
+        self::$db->query("DELETE FROM Parcel WHERE id = :id");
+        self::$db->bind('id', $id, null);
+        return self::$db->execute();
     }
 
     public static function load($id = null)
     {
+        self::$db->query("SELECT * FROM Parcel WHERE id = :id");
+        self::$db->bind('id', $id, null);
+        return self::$db->single();
     }
 
     public static function loadAll()
     {
+        self::$db->query("SELECT * FROM Parcel");
+        return self::$db->resultSet();
     }
 
     public static function setDb(Database $db)
     {
+        self::$db = $db;
     }
 
 
@@ -37,13 +69,6 @@ class Parcel implements Action
     {
         return $this->id;
     }
-
-
-    public function setId($id)
-    {
-        $this->id = $id;
-    }
-
 
     public function getSender()
     {
--- restEndPoints/Parcel.php
+++ restEndPoints/Parcel.php
@@ -1,2 +1,39 @@
 <?php
 
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    $tmpParcel = [];
+    if (isset($pathId) && !empty($pathId)) {
+        $parcel = Parcel::load($pathId);
+        $tmpParcel[0]['id'] = $parcel['id'];
+        $tmpParcel[0]['sender'] = $parcel['sender'];
+        $tmpParcel[0]['size'] = $parcel['size'];
+        $tmpParcel[0]['address'] = $parcel['address'];
+    } else {
+        $parcel = Parcel::loadAll();
+        foreach ($parcel as $k => $p) {
+            $tmpParcel[$k]['id'] = $p['id'];
+            $tmpParcel[$k]['sender'] = $p['sender'];
+            $tmpParcel[$k]['size'] = $p['size'];
+            $tmpParcel[$k]['address'] = $p['address'];
+
+        }
+    }
+    $response = $tmpParcel;
+
+} elseif ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    parse_str(file_get_contents("php://input"), $patch_vars);
+    $parcel = new Parcel($patch_vars['user_id'], $patch_vars['size_id'], $patch_vars['address_id']);
+    if ($parcel->save()/* and creditQuantity > price */) $response = ['add'];
+
+} elseif ($_SERVER['REQUEST_METHOD'] == 'PATCH') {
+    parse_str(file_get_contents("php://input"), $patch_vars);
+    $parcel = new Parcel($patch_vars['user_id'], $patch_vars['size_id'], $patch_vars['address_id']);
+    if ($parcel->update($patch_vars['id'])) $response = ['patch'];
+
+} elseif ($_SERVER['REQUEST_METHOD'] == 'DELETE') {
+    parse_str(file_get_contents("php://input"), $patch_vars);
+    if (Parcel::delete($patch_vars['id'])) $response = ['delete'];
+
+} else {
+    $response = ['error' => 'Wrong request method'];
+}
